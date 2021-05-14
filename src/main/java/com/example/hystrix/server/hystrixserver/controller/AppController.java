16d655ab6b3bd31edc78cbae1d6449af07b80a1c
deepakjayaprakash <deepakjpkodlekere@gmail.com>
2021-05-14 18:36:04 +0530

    random responses add

--- src/main/java/com/example/hystrix/server/hystrixserver/controller/AppController.java
+++ src/main/java/com/example/hystrix/server/hystrixserver/controller/AppController.java
@@ -1,6 +1,8 @@
 package com.example.hystrix.server.hystrixserver.controller;
 
 import com.example.hystrix.server.hystrixserver.dto.ResponseDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,10 +10,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping
 public class AppController {
+    Logger logger = LoggerFactory.getLogger(AppController.class);
 
     @RequestMapping(value = "/getSimpleResponse", method = RequestMethod.GET)
-    public ResponseDTO getSimpleResponse() {
+    public ResponseDTO getSimpleResponse() throws Exception {
         ResponseDTO responseDTO = new ResponseDTO();
-        return responseDTO;
+        int random = (int) (Math.random() * 100);
+        System.out.println(random);
+        if (random % 2 == 0) {
+            System.out.println("bad request");
+            throw new Exception();
+        } else {
+            System.out.println("good request");
+            return responseDTO;
+        }
     }
 }
